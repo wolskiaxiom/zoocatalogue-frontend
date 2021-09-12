a85faa6880cbe446aa0af72399f8427a14470d2e
Lukasz Wolski <wolskiaxiom@interia.pl>
2021-09-12 10:54:45 +0200

    try use fetch

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,13 +1,24 @@
-import {createContext, useContext, useReducer} from "react";
+import {createContext, useContext, useEffect, useReducer, useState} from "react";
 import {cartReducer} from "./Reducers";
 import mock from '../mocks/mock';
 
 const Cart = createContext()
 
-const Context = ({ children }) => {
+const Context = ({children}) => {
 
+    const [animals, setAnimals] = useState(null);
+
+    useEffect(async () => {
+        const response = await fetch('http://localhost:3333/getallanimals');
+        const data = await response.json();
+        setAnimals(data)
+    }, [])
+
+    //what I want
+    // const products = animals
+
+    // what I have
     const products = mock
-
     const [state, dispatch] = useReducer(cartReducer, {
         products: products,
         cart: []
@@ -24,4 +35,4 @@ export default Context
 
 export const CartState = () => {
     return useContext(Cart);
-} ;
+};
