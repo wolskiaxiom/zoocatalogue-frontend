08ab3bf8e539d0ba391f5d421ff645690982b43f
Lukasz Wolski <wolskiaxiom@interia.pl>
2021-09-12 09:59:37 +0200

    use mocked data instead of faker

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,23 +1,12 @@
 import {createContext, useContext, useReducer} from "react";
-import faker from 'faker';
 import {cartReducer} from "./Reducers";
+import mock from '../mocks/mock';
 
 const Cart = createContext()
-faker.seed(99);
 
 const Context = ({ children }) => {
 
-    const products = [...Array(20)]
-        .map(() => ({
-            id: faker.datatype.uuid(),
-            animal: faker.animal.dog(),
-            name: faker.commerce.productName(),
-            price: faker.commerce.price(),
-            image: faker.random.image(),
-            inStock: faker.random.arrayElement([0, 1, 2, 3, 4, 5, 6, 7]),
-            fastDelivery: faker.datatype.boolean(),
-            ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
-        }))
+    const products = mock
 
     const [state, dispatch] = useReducer(cartReducer, {
         products: products,
